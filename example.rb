9b36f3122d522678fd2e64f31c0e52c6ffe59322
Rab Beverly <rab@robertbeverly.com>
2015-03-18 12:05:04 -0400

    noodling

--- example.rb
+++ example.rb
@@ -6,5 +6,6 @@ loaded_csv = CSV.read('./orders.csv')
 orders = Hash_CSV.convert(loaded_csv)
 orders.each { |order| order[:address_string] = [order["Shipping Address1"], order["Shipping City"], order["Shipping State or Province"]].join(' ') 
   geo_address = Geocoder.search(order[:address_string])
-  puts geo_address.first.sub_state
+  geo_address[:county] = geo_address.first.sub_state
 }
+puts geo_address.inspect
