b4cafb9c23733301590826c27e95754db8b5e732
Rab Beverly <rab@robertbeverly.com>
2015-03-20 15:33:03 -0400

    output csv with tax info

--- example.rb
+++ example.rb
@@ -4,6 +4,7 @@ require './hash_csv.rb'
 
 ORDER_PATH = ENV['HOME'] + "/Box\ Sync/Tax\ Calculation/orders.csv"
 RATE_PATH = ENV['HOME'] + "/Box\ Sync/Tax\ Calculation/county-tax.csv"
+OUTPUT_PATH = ENV['HOME'] + "/Box\ Sync/Tax\ Calculation/report.csv"
 
 orders_csv = CSV.read(ORDER_PATH)
 orders = Hash_CSV.convert(orders_csv, true)
@@ -12,14 +13,28 @@ rates_csv = CSV.read(RATE_PATH)
 rates = Hash_CSV.convert(rates_csv, false)
 
 orders.each { |order| 
-  order[:address_string] = [order["Shipping Address1"], 
-    order["Shipping City"], 
-    order["Shipping State or Province"]].join(' ')
+  order[:address_string] = 
+    [order["Shipping Address1"].split.map(&:capitalize).join(' '), 
+      order["Shipping City"].downcase.capitalize, 
+      order["Shipping State or Province"]].join(' ')
   geo_address = Geocoder.search(order[:address_string])
   order[:county] = geo_address.first.sub_state
   order[:discretionary_tax] = rates[order[:county]]
 }
-puts orders.first.inspect
 
+# puts orders.first.inspect
 
+CSV.open(OUTPUT_PATH, "wb") do |csv|
+  csv << ["Order Number", "Name", "Address", "County", "Discretionary Tax"]
+  orders.each { |order| 
+    csv << [
+      order["Order number"], 
+      order["Billing First Name"].downcase.capitalize + 
+        " " + order["Billing Last Name"].downcase.capitalize,
+      order[:address_string],
+      order[:county],
+      order[:discretionary_tax]
+    ]
+  }
+end
 
