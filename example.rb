bfd74146cededf00fc46e308bb77cb9446448db7
Rab Beverly <rab@robertbeverly.com>
2015-03-20 13:36:55 -0400

    more noodling

--- example.rb
+++ example.rb
@@ -2,10 +2,19 @@ require 'csv'
 require 'geocoder'
 require './hash_csv.rb'
 
-loaded_csv = CSV.read('./orders.csv')
-orders = Hash_CSV.convert(loaded_csv)
-orders.each { |order| order[:address_string] = [order["Shipping Address1"], order["Shipping City"], order["Shipping State or Province"]].join(' ') 
-  geo_address = Geocoder.search(order[:address_string])
-  geo_address[:county] = geo_address.first.sub_state
-}
-puts geo_address.inspect
+ORDER_PATH = ENV['HOME'] + "/Box\ Sync/Tax\ Calculation/orders.csv"
+RATE_PATH = ENV['HOME'] + "/Box\ Sync/Tax\ Calculation/county-tax.csv"
+
+orders_csv = CSV.read(ORDER_PATH)
+orders = Hash_CSV.convert(orders_csv, true)
+
+rates_csv = CSV.read(RATE_PATH)
+rates = Hash_CSV.convert(rates_csv, false)
+puts rates
+# puts orders.inspect
+
+# orders.each { |order| order[:address_string] = [order["Shipping Address1"], order["Shipping City"], order["Shipping State or Province"]].join(' ') 
+#   geo_address = Geocoder.search(order[:address_string])
+#   geo_address[:county] = geo_address.first.sub_state
+# }
+# puts geo_address.inspect
--- hash_csv.rb
+++ hash_csv.rb
@@ -2,9 +2,23 @@
 
 class Hash_CSV
   # Pass in a CSV, ie: CSV.read('file/path')
-  def self.convert(c)
-    keys = c[0]
-    c.delete_at(0)
-    c.map { |item| Hash[keys.zip(item)]}
+  def self.convert(c, headers)
+    if (headers == true)
+      c_copy = c.dup
+      keys = c_copy[0]
+      values = c_copy.delete_at(0)
+      c_copy.map { |item| Hash[keys.zip(item)] }
+    else
+      Hash[*c.dup.flatten]
+    end
+  end
+end
+
+class Array
+  def odds
+    self.values_at(* self.each_index.select {|i| i.odd?})
+  end
+  def evens
+    self.values_at(* self.each_index.select {|i| i.even?})
   end
 end
